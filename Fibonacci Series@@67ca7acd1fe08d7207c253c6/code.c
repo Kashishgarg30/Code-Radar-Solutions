ba3f64943fcb5fa6061f9c8e5f0d2a1e8a870c48
Kashishgarg30 <kashish6284034221@gmail.com>
2025-03-30 23:01:56 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    if(n<=0){
+        printf("Invalid imput\n");
+        return 0;
+    }
+        fibonacciSeries(n);
+        return 0;
+    }
